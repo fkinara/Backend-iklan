30737ea3389f856509251ca488d4b65041ae2eec
fkinara <42002518+fkinara@users.noreply.github.com>
2020-03-26 21:32:30 +0700

    success

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,11 @@ Route::post('login', 'UserController@login');
 
 Route::middleware(['jwt.verify'])->group(function(){
     Route::get('user', 'UserController@getAuthenticatedUser');
+
+    //iklan
+    Route::get('iklan', "IklanController@index"); //read iklan
+	Route::get('iklan/{limit}/{offset}', "IklanController@getAll"); //read iklan
+	Route::post('iklan', 'IklanController@store'); //create iklan
+	Route::put('iklan/{id}', "IklanController@update"); //update iklan
+	Route::delete('iklan/{id}', "IklanController@delete"); //delete iklan
 });
